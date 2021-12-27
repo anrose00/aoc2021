49c26a4f7d3848d661d7fb69ee523df8fc26f7f6
Andreas Rosenberg <andreas.rosenberg@apis.de>
2021-12-27 18:03:33 +0100

    day 7

---
+++ day7/main2.cpp
@@ -0,0 +1,126 @@
+/* https://adventofcode.com/2021/day/7 - Part 2 */
+
+#include <istream>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iomanip>
+#include <cctype>
+#include <algorithm>
+
+#define CHAR_SPACE 32
+
+using std::vector;
+using vNumbers = vector<int>;
+
+// debug helper to print vector using <<
+std::ostream & operator<<(std::ostream &os, const vNumbers& obj) { 
+    for(const auto& num: obj) std::cout << num << ' ';
+    return os;
+}
+
+// debug helper to print pair<int,int> using <<
+std::ostream & operator<<(std::ostream &os, const std::pair<int,int>& obj) { 
+    os << '[' << obj.first << ',' << obj.second << ']';
+    return os;
+}
+
+// tokenize input string and answer a vector of integers
+vector<int> tokenizeNumbers(const std::string &input)
+{
+    vNumbers tokens = vNumbers();
+    int next=0,last = 0;
+    int inputSize = input.size();
+    while (next<inputSize)
+    {
+        int num;
+        while (isdigit(input[next]) && next<inputSize) next++;
+        num = stoi(input.substr(last,next-last),nullptr,10);
+        tokens.push_back(num);
+        while (!isdigit(input[next]) && next<inputSize) next++;
+        last = next;
+    }
+    return tokens;    
+}
+
+bool isOnlyWhiteSpace(std::string _str)
+{
+    for (const auto& c : _str)
+    {
+        if (c > CHAR_SPACE )
+            return false;
+    }
+    return true;
+}
+
+int getMax(int start, const vNumbers _vn)
+{
+    int res=start;
+    for (auto e : _vn)
+        res = std::max(res,e);
+    return res;
+}
+
+std::pair<int,int> getMinIndex(int start, const vNumbers _vn)
+{
+    int res=start;
+    int index;
+    int end =_vn.size();
+    for (auto i=0;i<end;i++)
+    {
+        if (_vn[i] < res)
+        {
+            res = _vn[i];
+            index = i;
+        }
+    }
+    return std::make_pair(res,index);
+}
+
+int crabFuel(int in)
+{
+    int out=0;
+    for (auto i=0;i<=in;i++)
+        out += i;
+    return out;
+}
+
+int getFuel(const vNumbers _vn, int ref)
+{
+    int fuel=0;
+    for (auto i : _vn)
+        fuel += crabFuel(std::abs(ref - i));
+    return fuel;
+}
+
+int main(int argc, char *argv[])
+{
+    std::string numberString;
+    std::fstream stream;
+    vNumbers distances;
+    vNumbers fuel;
+    
+    int max=0;
+    if (argc == 2)
+    {
+        stream.open(argv[1],std::ios::in);
+        while (!stream.eof())
+        {
+            std::getline(stream,numberString);
+            if (!isOnlyWhiteSpace(numberString))
+            {
+                distances = tokenizeNumbers(numberString);
+            }
+        }
+        stream.close();
+        
+        max = getMax(0,distances);
+        for (auto i=0;i<=max;i++)
+        {
+            fuel.push_back(getFuel(distances,i));
+        }
+
+        std::cout << "Result: "<< getMinIndex(0x7FFFFFFF,fuel) << '\n';
+    }
+}
